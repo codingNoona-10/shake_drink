55a887a6ac8c2d5af1945dfc40e7afc238dbb2cf
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-17 21:25:39 +0900

    Feat: SearchPage 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />
-			{/* <Route path="/" element={<DetailPage />} /> */}
+			<Route path="/search" element={<SearchPage />} />
 		</Routes>
 	);
 }
