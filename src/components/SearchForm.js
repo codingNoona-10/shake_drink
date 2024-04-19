511253ca4c3b6c564088a8e29e18943ec20f604b
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-19 19:59:21 +0900

    Feat: 검색폼 기능 구현/ Style: CSS 수정

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import { Box, TextField, InputAdornment, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import ReplayIcon from "@mui/icons-material/Replay";
+import { useNavigate } from "react-router-dom";
 
 const SearchForm = ({ onSearch }) => {
 	const [keyword, setKeyword] = useState("");
+	const navigate = useNavigate();
 
 	const searchByKeyword = (e) => {
 		e.preventDefault();
 		console.log("@@@ 검색 키워드:", keyword);
+		navigate(`?q=${keyword}`, { replace: true });
 		onSearch(keyword);
-		setKeyword("");
+		// setKeyword("");
+	};
+
+	const handleClearInput = () => {
+		setKeyword(""); // 입력 필드 초기화
+		navigate("/search", { replace: true });
 	};
 
 	return (
@@ -36,6 +45,7 @@ const SearchForm = ({ onSearch }) => {
 					variant="outlined"
 					placeholder="Search"
 					color="warning"
+					disableClearable
 					sx={{
 						// borderColor: "#FD4926",
 						border: "1px solid #FD4926",
@@ -44,11 +54,27 @@ const SearchForm = ({ onSearch }) => {
 						"& .MuiOutlinedInput-root": {
 							borderRadius: "15px",
 							height: 70,
+							"& .MuiInputBase-input": {
+								pl: 2,
+							},
 						},
 						"&:-webkit-autofill": {
 							borderRadius: "15px",
 							WebkitBoxShadow: "0 0 0 1000px #FD4926 inset;",
 							WebkitTextFillColor: "#FD4926",
+						},
+						'& input[type="search"]::-webkit-search-cancel-button': {
+							WebkitAappearance: "none",
+							appearance: "none",
+						},
+						"&:hover": {
+							borderColor: "#FD4926",
+						},
+						"&.Mui-focused": {
+							borderColor: "#FD4926",
+						},
+						"&.Mui-disabled": {
+							borderColor: "#FD4926",
 						},
 					}}
 					// InputLabelProps={{
@@ -66,9 +92,16 @@ const SearchForm = ({ onSearch }) => {
 								<IconButton
 									type="submit"
 									aria-label="search"
-									sx={{ backgroundColor: "rgb(242, 92, 92)" }}
+									sx={{ backgroundColor: "#FD4926", borderRadius: "8px" }}
 								>
 									<SearchIcon sx={{ color: "#fff" }} fontSize="large" />
+								</IconButton>
+							</InputAdornment>
+						),
+						endAdornment: keyword && ( // 입력 값이 있을 때만 클리어 아이콘 표시
+							<InputAdornment position="end">
+								<IconButton onClick={handleClearInput} aria-label="clear input">
+									<ReplayIcon sx={{ color: "#aaa" }} fontSize="large" />
 								</IconButton>
 							</InputAdornment>
 						),
