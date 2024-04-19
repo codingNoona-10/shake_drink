9d6d8bc2e1552e97983377cf27c3f88c16beaede
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-19 20:37:34 +0900

    Feat: 서치페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import DetailPage from "./pages/DetailPage";
+import UserPage from "./pages/UserPage";
+// import AppLayout from "./layout/AppLayout";
+import AppLayout from "./layout/AppLayout";
 import SearchPage from "./pages/SearchPage";
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/search" element={<SearchPage />} />
+			<Route path="/" element={<AppLayout />}>
+				<Route index element={<HomePage />} />
+				<Route path="detail" element={<DetailPage />} />
+				<Route path="user" element={<UserPage />} />
+				<Route path="/search" element={<SearchPage />} />
+			</Route>
+			{/* <Route path="/" element={<DetailPage />} /> */}
 		</Routes>
 	);
 }
